9ef7b77941c0aca9ffa8ce2f79d6ed8e0aafc0e2
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2022-09-18 21:56:08 -0400

    JS Test

--- buttonScript.js
+++ buttonScript.js
@@ -1,4 +1,11 @@
 
-function getData() {
-    document.getElementById("weatherData").innerHTML = Date();
+// https://stackoverflow.com/questions/36975619/how-to-call-a-rest-web-service-api-from-javascript
+
+async function getData() {
+    const req = await fetch('https://httpbin.org/get');
+    const json = await response.json()
+    document.getElementById("weatherData").innerHTML = json;
 }
+
+// IP: http://checkip.dyndns.org/
+// GET: https://httpbin.org/get
