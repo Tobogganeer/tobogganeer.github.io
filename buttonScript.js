c3e77eedc7f091ce47c4147d7d7017ceea325da5
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2022-09-19 21:36:36 -0400

    Update

--- buttonScript.js
+++ buttonScript.js
@@ -2,6 +2,7 @@
 // https://stackoverflow.com/questions/36975619/how-to-call-a-rest-web-service-api-from-javascript
 
 async function getData() {
+    document.getElementById("weatherData").innerHTML = "Fetching..."
     const req = await fetch('https://httpbin.org/get');
     const json = await req.json()
     document.getElementById("weatherData").innerHTML = JSON.stringify(json);
